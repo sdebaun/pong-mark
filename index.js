ae47b26b2fcf82f81ec22e5e379a28982c02b3f7
Stevo <sdebaun@sparks.network>
2015-07-13 12:45:21 -0700

    never compiled cs :O

--- index.js
+++ index.js
@@ -8,46 +8,19 @@
 
   pongular.module('pong-mark', ['pong-q']).service('postmark', function() {
     return require('Postmark');
-  }).service('sendTo', function($promise, postmark) {
+  }).service('configuredPostmark', function(postmark, postmarkConfig) {
+    return postmark(postmarkConfig.apiKey);
+  }).service('$mailer', function($promise, configuredPostmark) {
     return function(attrs) {
       return $promise(function(d) {
-        return postmark.send(attrs, function(error, success) {
+        return configuredPostmark.send(attrs, function(error, success) {
           if (error) {
-            console.log('Postmark Error:', error);
+            return d.reject(error.message);
+          } else {
+            return d.resolve();
           }
-          return error && d.reject(error.message) || d.resolve();
         });
       });
-    };
-  }).service('$mailComposer', function($promise) {
-    return function(config, composerFn) {
-      return $promise(function(d) {
-        var err;
-        try {
-          composerFn(config);
-          return d.resolve();
-        } catch (_error) {
-          err = _error;
-          return d.reject(err);
-        }
-      });
-    };
-  }).service('mailComposer', function() {
-    return {
-      init: function(config1) {
-        this.config = config1;
-      },
-      buildAttrs: function(params) {
-        this.params = params;
-        return {
-          From: "volunteer@sparks.network",
-          To: profile.name + " <" + profile.emailAddress + ">",
-          ReplyTo: "" + replyTo,
-          Subject: eventId + " Volunteer: " + subject,
-          HtmlBody: messageWrapper(profile, message),
-          Tag: "" + eventId
-        };
-      }
     };
   });
 
